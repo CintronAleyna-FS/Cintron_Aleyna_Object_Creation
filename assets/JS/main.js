6788a681a1826c4a30c782fa75239ddbbf4f57ed
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2021-10-11 18:00:41 -0500

    Created form

---
+++ assets/JS/main.js
@@ -0,0 +1,29 @@
+// At least 3 input fields strings and numbers
+
+class Movie{
+
+}
+
+
+
+
+
+// Majority of logic
+class Main {
+    
+    constructor(){
+        
+    }
+    // Confirmation message
+
+    // Display button
+
+    // Add button
+
+    // Form validation
+}
+
+(()=>{
+    const main = new Main();
+    }
+)();
---
+++ assets/JS/utils.js
@@ -0,0 +1,5 @@
+class Utility{
+    static x(x){
+        return x
+    }
+}

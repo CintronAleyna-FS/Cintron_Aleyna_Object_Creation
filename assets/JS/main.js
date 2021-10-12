d68ea9f4a0211d952d3581c9d28bfd73fa3e9b18
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2021-10-11 22:49:44 -0500

    completed.

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -24,9 +24,12 @@ class Main {
         document.querySelector("#addBtn").addEventListener("click", e=> this.add(e));
 
         document.querySelector("#displayBtn").addEventListener("click", e=> this.display(e));
+
+        document.querySelector("#name").addEventListener("click", e=> this.clear(e));
     }
-    
-    // Form validation
+    clear(){
+        document.querySelector("#confirm-msg").innerHTML = ""
+    }
     add(e){
         const name=document.querySelector("#name");
         const age=document.querySelector("#age");
@@ -35,11 +38,23 @@ class Main {
             e.preventDefault(); 
             let char = new Character(name.value, age.value, talent.value);
             this.chars.push(char)
+            document.querySelector("#confirm-msg").innerHTML = `A new character has been added!`
+            document.querySelector("#confirm-msg").style.visibility = "visible";
         }
     }
     // Display button
     display(e){
-
+        document.querySelector("#output").innerHTML = "";
+        this.chars.forEach(char=>{
+        
+        for(let prop in char){
+            let ul = document.querySelector("#output");
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(`${prop}: ${char[prop]} `));
+            ul.appendChild(li);
+            document.querySelector("#output").style.visibility = "visible";
+        }
+       })
     }   
 }
 
